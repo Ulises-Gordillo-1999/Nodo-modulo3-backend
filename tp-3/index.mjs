6859dcd7edd23f8f1c554fd263eb86d74f134150
ulises <uligordillo@gmail.com>
2025-05-24 22:11:24 -0300

    practica trabajo practico 3

---
+++ tp-3/index.mjs
@@ -0,0 +1,12 @@
+import { leerSuperheroes, agregarSuperheroes } from './utils.mjs';
+
+
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+//agregar nuevos superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+// Leer y mostrar la lista de superhéroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
+console.log('Superhéroes ordenados:');
+console.log(superheroes);
